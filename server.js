8ef34a72ed062bba2fd3fc8c85ef356bb96aa729
Akshay-VK <akshaykaruvathil@gmail.com>
2021-04-05 12:58:23 +0530

    fixed the unintentional but in server cde

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = process.env.port || 3000;
 
 app.use(express.static(__dirname+'/public'));
 
-app.listen(port =>{
+app.listen(port ,()=>{
     console.log('Server started at port '+port);
 });
